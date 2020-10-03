7cde0c139cab49cc138b2635c7cd7dda753d0232
EIYA-ASUS <EIYA5498513@noreply.github.com>
2020-10-03 15:35:19 +0900

    `benchmark_jobs` ADD INDEX idx4 (`status`,`id`);

--- sql/schema.sql
+++ sql/schema.sql
@@ -41,6 +41,7 @@ CREATE TABLE `benchmark_jobs` (
 ALTER TABLE `benchmark_jobs` ADD INDEX idx1 (`team_id`,`id`);
 ALTER TABLE `benchmark_jobs` ADD INDEX idx2 (`status`,`team_id`,`id`);
 ALTER TABLE `benchmark_jobs` ADD INDEX idx3 (`status`,`team_id`,`finished_at`);
+ALTER TABLE `benchmark_jobs` ADD INDEX idx4 (`status`,`id`);
 
 DROP TABLE IF EXISTS `clarifications`;
 CREATE TABLE `clarifications` (
