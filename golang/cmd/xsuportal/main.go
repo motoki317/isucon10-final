5962e55fc2d7e290347f3a52b9996d9390e7e777
EIYA-ASUS <EIYA5498513@noreply.github.com>
2020-10-03 13:15:28 +0900

    TeamCapacity               = 30

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -39,7 +39,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 10
+	TeamCapacity               = 30
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
