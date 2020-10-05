2d6ddcc51c4a52710b46fe20dc0529bfa5cbcd50
Shu Takayama <syu.takayama@gmail.com>
2020-10-06 02:23:57 +0900

    fix

--- golang/cmd/benchmark_server/main.go
+++ golang/cmd/benchmark_server/main.go
@@ -236,7 +236,7 @@ func main() {
 	//}()
 
 	port := util.GetEnv("PORT", "50051")
-	address := ":" + port
+	address := "0.0.0.0:" + port
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
